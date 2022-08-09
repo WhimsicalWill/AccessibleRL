db5dc811585120795761a67e5e6bea15d7411e53
whimsicalwill <knipewilliam20@gmail.com>
2022-08-09 01:06:23 -0400

    Remove other experiments

--- cartpole_control_main.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import gym
-from control_cartpole import Agent
-
-class CartPoleStateDigitizer():
-    def __init__(self, bounds=(2.4, 4, 0.209, 4), n_bins=10)
