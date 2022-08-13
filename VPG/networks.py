9fcb530d57c48c0ca3d9b7d162f57c8affc6d24a
whimsicalwill <knipewilliam20@gmail.com>
2022-08-12 23:35:37 -0400

    Separate actor and value networks

--- VPG/networks.py
+++ VPG/networks.py
@@ -4,27 +4,45 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
-class ActorValue(nn.Module):
-	def __init__(self, lr, input_dims, n_actions, name, fc1_dims=256, fc2_dims=256, chkpt_dir='tmp/vpg'):
-		super(ActorValue, self).__init__()
+class Actor(nn.Module):
+	def __init__(self, alpha, input_dims, n_actions, fc1_dims=256, chkpt_dir='tmp/vpg'):
+		super(Actor, self).__init__()
 		self.fc1 = nn.Linear(*input_dims, fc1_dims)
-		self.fc2 = nn.Linear(fc1_dims, fc2_dims)
-		self.pi = nn.Linear(fc2_dims, n_actions)
-		self.v = nn.Linear(fc2_dims, 1)
+		self.pi = nn.Linear(fc1_dims, n_actions)
   
-		self.chkpt_file = f"{chkpt_dir}/vpg_chkpt"
+		self.chkpt_file = f"{chkpt_dir}/vpg_actor"
   
-		self.optimizer = optim.Adam(self.parameters(), lr=lr)
+		self.optimizer = optim.Adam(self.parameters(), lr=alpha)
 		self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 		self.to(self.device)
 
 	def forward(self, state): 	 
 		x = F.relu(self.fc1(state))
-		x = F.relu(self.fc2(x))
 		action_probs = F.softmax(self.pi(x))
-		state_value = self.v(x)
-	
-		return action_probs, state_value
+		return action_probs
+
+	def save_checkpoint(self):
+		torch.save(self.state_dict(), self.chkpt_file)
+
+	def load_checkpoint(self):
+		self.load_state_dict(torch.load(self.chkpt_file))
+
+class Value(nn.Module):
+	def __init__(self, beta, input_dims, fc1_dims=256, chkpt_dir='tmp/vpg'):
+		super(Value, self).__init__()
+		self.fc1 = nn.Linear(*input_dims, fc1_dims)
+		self.value = nn.Linear(fc1_dims, 1)
+
+		self.chkpt_file = f"{chkpt_dir}/vpg_value"
+
+		self.optimizer = optim.Adam(self.parameters(), lr=beta)
+		self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+		self.to(self.device)
+
+	def forward(self, state): 	 
+		x = F.relu(self.fc1(state))
+		state_value = F.softmax(self.value(x))
+		return state_value
 
 	def save_checkpoint(self):
 		torch.save(self.state_dict(), self.chkpt_file)
